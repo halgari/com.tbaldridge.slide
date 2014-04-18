9d95653eef9a38d43178b7f756272c324b608035
Timothy Baldridge <tbaldidge@gmail.com>
2014-04-18 15:24:52 -0600

    more working on bindings and other layouts

--- src/com/tbaldridge/slide.clj
+++ src/com/tbaldridge/slide.clj
@@ -1,17 +1,17 @@
 (ns com.tbaldridge.slide
   (:require [com.tbaldridge.slide.util :as util]
-            [clojure.core.async :refer [go <! >! chan timeout put! sliding-buffer map< pipe dropping-buffer]])
+            [clojure.core.async :refer [go <! >! chan timeout put! sliding-buffer map< pipe dropping-buffer] :as async])
   (:import [javax.swing JFrame]
            [javafx.collections FXCollections]
            [javafx.event ActionEvent EventHandler]
            [javafx.scene Scene SceneBuilder]
            [javafx.scene.control Button ListView]
-           [javafx.scene.layout StackPane]
+           [javafx.scene.layout StackPane BorderPane]
            [javafx.stage Stage StageBuilder]
            [javafx.application Application]
            [javafx.beans.value ChangeListener ObservableValue]
            (javafx.scene.control ButtonBuilder TextFieldBuilder ListViewBuilder)
-           (javafx.scene.layout VBoxBuilder)
+           (javafx.scene.layout VBoxBuilder BorderPaneBuilder)
            [java.lang.ref WeakReference]))
 
 (def ^:dynamic *builder-mappings*
@@ -19,6 +19,7 @@
    :scene SceneBuilder
    :button ButtonBuilder
    :vbox VBoxBuilder
+   :border-pane BorderPaneBuilder
    :list-view ListViewBuilder
    :text-field TextFieldBuilder})
 
@@ -47,6 +48,24 @@
 
        :else v))
 
+
+(defmulti data-converter identity)
+
+(defmethod data-converter javafx.beans.property.SimpleStringProperty
+  [x]
+  (fn [data]
+    (str data)))
+
+(defmethod data-converter javafx.scene.Node$MiscProperties$8
+  [x]
+  (fn [data]
+    (boolean data)))
+
+(defmethod data-converter javafx.scene.control.ListView$2
+  [x]
+  (fn [data]
+     (FXCollections/observableArrayList data)))
+
 (defn output-binding? [nm]
   (.endsWith (name nm) "->"))
 
@@ -60,23 +79,27 @@
     (.addListener ctrl-prop
                   (reify ChangeListener
                     (changed [this cl old-val new-val]
-                      (put! v new-val))))))
+                      (put! v (or new-val :nil)))))))
 
 (defn create-input-binding [crtl k c]
   (let [sk (name k)
         nm (subs sk 0 (- (count sk) 2))
-        ctrl-prop (eval-call `(fn [ctrl#] (. ctrl# ~(symbol (str nm "Property")))) crtl)]
+        ctrl-prop (eval-call `(fn [ctrl#] (. ctrl# ~(symbol (str nm "Property")))) crtl)
+        converter (data-converter (class ctrl-prop))]
         (go
           (try
             (loop []
               (when-let [v (<! c)]
                 (println v)
-                (util/run-later (.setValue ctrl-prop (to-javafx-value v)))
+                (util/run-later (.setValue ctrl-prop (converter v)))
                 (recur)))
             (catch Throwable ex
               (println ex))))))
 
-(defn build-item [desc]
+(defmulti build-item :type)
+
+(defmethod build-item :default
+  [desc]
   (if (map? desc)
     (let [builder (get *builder-mappings* (:type desc))
           _ (assert builder (str "Could not find builder for " (pr-str desc)))
@@ -99,49 +122,57 @@
         built))
     desc))
 
-(defn build-scene [desc]
-  (let [state (atom {})
-        root (build-item state desc)]
-    (doseq [[ctrl props] (:bind @state)
-            [prop binds] props
-            bind binds]
-      (let [ctrl-prop (eval-call `(fn [ctrl#] (. ctrl# ~(symbol (str (name prop) "Property")))) @ctrl)
-            c (complete-channel bind (:named @state))]
-        (go
-          (try
-            (loop []
-              (when-let [v (<! c)]
-                (println v)
-                (util/run-and-wait (.setValue ctrl-prop (to-javafx-value v)))
-                (recur)))
-            (catch Throwable ex
-              (println ex))))))
-    root))
-
 (let [topics (atom {})]
 
   (defn publish [topic msg]
     (println topic msg)
     (let [subs (get @topics topic)]
       (doseq [sub subs]
-        (put! sub msg))))
+        (when-let [c (.get sub)]
+          (put! c msg)))))
 
   (defn publish-to [topic]
     (let [c (chan (dropping-buffer 1024))]
       (go
-        (loop []
-          (when-let [v (<! c)]
-            (publish topic v)
-            (recur))))
+        (try
+          (loop []
+            (println "r")
+            (when-let [v (<! c)]
+              (publish topic v)
+              (recur))
+            (println "got nil"))
+          (catch Throwable ex
+            (println ex))))
       c))
 
   (defn subscribe-to [topic]
     (let [c (chan (dropping-buffer 1024))]
-      (swap! topics update-in [topic] (fnil conj #{}) c)
+      (swap! topics update-in [topic] (fnil conj #{}) (WeakReference. c))
       c))
 
   (defn get-topics []
     @topics))
+
+
+(defn bind-to-get-in [a path]
+  (let [c (chan (dropping-buffer 1))]
+    (add-watch a c (fn [k r o n]
+                     (when-let [v (get-in @a path)]
+                       (if (not (put! c v))
+                         (remove-watch a c)))))
+    (async/unique c)))
+
+(defn bind-to-assoc-in [a path]
+  (let [c (chan (async/sliding-buffer 1))]
+    (go (loop []
+          (let [v (<! c)]
+            (when-not (nil? v)
+              (swap! a assoc-in path v)
+              (recur)))))
+    c))
+
+(def a (atom {:foo "1"
+              :disable false}))
 
 (def scene {:type :stage
             :title "hello"
@@ -152,11 +183,25 @@
                            :minHeight 400
                            :minWidth 400
                            :children [{:type :button
-                                       :text<- (subscribe-to [:text])}
+                                       :text<- (bind-to-get-in a [:foo])
+                                       :pressed-> (publish-to :button-clicked2)}
                                       {:type :text-field
                                        :name :input
-                                       :text-> (publish-to [:text])
+                                       :text-> (bind-to-assoc-in a [:foo])
                                        :text "FooBar"}
+                                      {:type :border-pane
+                                       :top {:type :button
+                                             :text "Hey"}
+                                       :center {:type :list-view
+                                                :items<- (async/map<
+                                                          #(map (fn [x]
+                                                                  (build-item {:type :button
+                                                                               :text x})) %)
+                                                          (bind-to-get-in a [:items]))}
+                                       :left {:type :button
+                                              :text "cool"}
+                                       :right {:type :button
+                                               :text "right on"}}
                                       #_{:type :text-field
                                        :text (map-property
                                               (partial str "foo - ")
@@ -166,4 +211,5 @@
 
 (util/run-and-wait (.show (build-item scene)))
 
-(println (get-topics))
+(swap! a assoc :disable false)
+(println @a)
